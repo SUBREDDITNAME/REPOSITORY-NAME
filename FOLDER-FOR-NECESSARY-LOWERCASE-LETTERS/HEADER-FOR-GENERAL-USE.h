bde8469de65fb5703e3bb80484fede51142a3758
Wita5237 <aarondoverfordrive@outlook.com>
2026-09-15 10:19:32 -0700

    COMMIT THAT RENAMES ALL THE FILES

---
+++ FOLDER-FOR-NECESSARY-LOWERCASE-LETTERS/HEADER-FOR-GENERAL-USE.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#define RETURN_DIRECTIVE return;
+typedef int DATA_TYPE;
+typedef char OTHER_DATA_TYPE;
+typedef void THIRD_DATA_TYPE;
+#define LARGE_DATA_TYPE struct
+#define VALUE 0
+#define OTHER_VALUE 1
+#define SMALL_NUMBER 4
+#define MEDIUM_NUMBER 7
+#define LARGE_NUMBER 9
+#define CONDITIONAL_WORD if
+
+
+
---
+++ FOLDER-FOR-NECESSARY-LOWERCASE-LETTERS/HEADER-FOR-MAIN-FILE-ONLY.h
@@ -0,0 +1,7 @@
+#include "HEADER-FOR-GENERAL-USE.h"
+
+DATA_TYPE MAIN_FUNCTION(DATA_TYPE ARGUMENT_COUNT, OTHER_DATA_TYPE* ARGUMENT_TABLE[]);
+
+int main(int argc, char* argv[]) {
+    return MAIN_FUNCTION(argc, argv);
+}
